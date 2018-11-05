6992bc559169876ebfbe71c7dd59c48e5963bb43
Sai <rao.sharath29@gmail.com>
2018-11-05 13:00:31 +0530

    Charan's changes

--- NivasIt/ClientApp/app/components/courses/courses.component.ts
+++ NivasIt/ClientApp/app/components/courses/courses.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    templateUrl: './courses.component.html'
+    templateUrl: './courses.component.html',
+    styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
 }
