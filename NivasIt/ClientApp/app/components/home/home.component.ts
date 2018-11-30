f539ca04f47b8c75943a2e90db4685dacc70e4eb
Sai <rao.sharath29@gmail.com>
2018-12-01 01:13:43 +0530

    font icons added

--- NivasIt/ClientApp/app/components/home/home.component.ts
+++ NivasIt/ClientApp/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'home',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
 	styleUrls:['./home.component.css']
 })
 export class HomeComponent {
+    constructor(private router: Router) {
+
+    }
+    public knowmore() {
+        this.router.navigate(['/courses']); }
 }
